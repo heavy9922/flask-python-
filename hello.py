86d3ceb9679a77b4dfc7221783c2abbb0749a6e9
yeferson castiblanco <yeferson.castiblanco@inoutdelivery.com>
2023-03-29 14:32:11 -0500

    add se creo la estructura de jinja con flask

---
+++ hello.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, url_for
+from markupsafe import escape
+from datetime import datetime
+
+app = Flask(__name__)
+
+# filtros personalizados
+
+
+@app.add_template_filter
+def today(date):
+    return date.strftime('%d/%m/%Y')
+
+
+# app.add_template_filter(today, 'today')
+@app.add_template_global
+def repeat(s, n):
+    return s * n
+
+
+@app.route('/')
+def index():
+    name = 'yeferson'
+    friends = ['paola', 'alex', 'javier']
+    date = datetime.now()
+    return render_template('index.html', name=name, friends=friends, date=date)
+
+
+@app.route("/hello")
+@app.route("/hello/<name>")
+@app.route("/hello/<name>/<int:age>/<email>")
+def hello(name=None, age=None, email=None):
+    my_data = {
+        'name': name,
+        'age': age,
+        'email': email
+    }
+    return render_template('hello.html', data=my_data)
+
+# escape de html
+
+
+@app.route('/code/<path:code>')
+def code(code):
+    return f'<code>{escape(code)}</code>'
